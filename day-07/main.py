96657ed817e4252e98d9f3a8e646eacc39d3b040
Mikael Escolin <mikaelescolin@gmail.com>
2020-12-22 18:47:31 +0000

    Add solution for day 07 part 2

--- day-07/main.py
+++ day-07/main.py
@@ -16,21 +16,6 @@ def main():
 # ENDM main()
 
 
-def check_for_gold(bags_dict: Dict[str, List[str]], bags: List[str]) -> bool:
-    while len(bags) != 0:
-        curr_bag = bags.pop()
-        inside = bags_dict[curr_bag]
-        if "shiny gold" in inside:
-            return True
-        else:
-            bags += bags_dict[curr_bag]
-        # END IF
-    # END WHILE
-
-    return False
-# END check_for_gold()
-
-
 def part_one():
     if (len(sys.argv) < 2):
         print("ERROR: no input file")
@@ -58,10 +43,17 @@ def part_one():
 
         count = 0
         for bag in bags_dict.keys():
-            has_gold = check_for_gold(bags_dict, [bag])
-            if has_gold == True:
-                count += 1
-            # END IF
+            bags = [bag]
+            while len(bags) != 0:
+                curr_bag = bags.pop()
+                inside = bags_dict[curr_bag]
+                if "shiny gold" in inside:
+                    count += 1
+                    break
+                else:
+                    bags += bags_dict[curr_bag]
+                # END IF
+            # END WHILE
         # END FOR
         print(count)
     # END WITH
@@ -69,15 +61,50 @@ def part_one():
 
 
 def part_two():
-    """
     if (len(sys.argv) < 2):
         print("ERROR: no input file")
         exit(0)
     # END IF
 
     with open(sys.argv[1], "r", encoding="utf-8") as file:
+        bags_dict: Dict[str, List[str]] = {}
+
+        for line in file:
+            container, bags = line.split("contain")
+            container: str = container.replace("bags", "").strip()
+            bags: List[str] = [bag.replace("bags", "").replace("bag", "").strip()
+                               for bag in bags.strip(".\n").split(",")]
+
+            # print(container, bags)
+            if bags[0] == "no other":
+                bags_dict[container] = []
+            else:
+                temp = []
+                for bag in bags:
+                    index = bag.find(" ")
+                    temp.append((int(bag[:index]), bag[index + 1:]))
+                # END FOR
+                bags = temp
+                bags_dict[container] = bags
+            # END IF
+        # END FOR
+
+        # pp(bags_dict)
+
+        count = 0
+        bags = bags_dict["shiny gold"]
+
+        while len(bags) != 0:
+            curr_bag = bags.pop()
+
+            amount, type = curr_bag
+            count += amount
+
+            for i in range(amount):
+                bags += bags_dict[type]
+        # END WHILE
+        print(count)
     # END WITH
-    """
 # END part_two()
 
 
